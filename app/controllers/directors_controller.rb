c66be4761da6d278dc4ebd4ec41f63831b00dd57
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-11-04 06:13:20 +0000

    completed actors, youngest and eldest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -11,4 +11,30 @@ class DirectorsController < ApplicationController
     render ({ :template => "director_templates/details"})
   end
 
+  def young
+    exclude_nil = Director.where.not({ :dob => nil })
+    @youngest_dob = exclude_nil.maximum(:dob)
+    matching_record = Director.where({ :dob => @youngest_dob})
+    @youngest_dir = matching_record.at(0).name
+    @youngest_id = matching_record.at(0).id
+    render ({ :template => "director_templates/young"})
+  end
+
+  def old
+    exclude_nil = Director.where.not({ :dob => nil })
+    @eldest_dob = exclude_nil.minimum(:dob)
+    matching_record = Director.where({ :dob => @eldest_dob})
+    @eldest_dir = matching_record.at(0).name
+    @eldest_id = matching_record.at(0).id
+    render ({ :template => "director_templates/old"})
+  end
+
+  def show
+    the_id = params.fetch("the_id")
+    matching_records = Director.where({:id => the_id})
+    @the_director = matching_records.at(0)
+
+    render ({ :template => "director_templates/details"})
+  end
+
 end
